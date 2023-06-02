dbc6792b1f761ea0d08e38ae07dc322cd49424b3
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-02 14:10:50 -0300

    crud-completed-service

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -25,6 +25,59 @@ const serviceController = {
         } catch (error) {
             console.log(error);
         }
+    },
+    get: async (req, res) => {
+        try {
+            const id = req.params.id
+            const service = await ServiceModel.findById(id);
+
+            if (!service) {
+                res.status(404).json({ msg: "Registo não encontrado!" });
+                return
+            }
+
+            res.json(service)
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id
+            const service = await ServiceModel.findById(id)
+
+            if (!service) {
+                res.status(404).json({ msg: "Não foi possivel excluir!" });
+                return
+            }
+
+            const deleteService = await ServiceModel.findByIdAndRemove(id);
+            
+            res.status(200).json({deleteService, msg: "Não foi possivel excluir!" });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    update: async (req, res) => {
+        const id = req.params.id;
+        // 1. Passo. Recebe os dados
+        const service = {
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            image: req.body.image
+        }
+
+        const updatedService = await ServiceModel.findByIdAndUpdate(id, service);
+
+        // 2. Passo. caso não encontrado
+        if (!updatedService) {
+            res.status(404).json({ msg: "Serviço não pode ser atualizado!" });
+            return;
+        }
+
+        // 3. Passo. atualiza caso tenha achado os dados
+        res.status(200).json({ service, msg: "Serviço atualizado com sucesso." });
     }
 };
 module.exports = serviceController;
--- routes/services.js
+++ routes/services.js
@@ -2,10 +2,24 @@ const router = require("express").Router()
 
 const serviceController = require("./../controllers/serviceController");
 
-router
+router // INSERIR
     .route("/services")
     .post((req, res) => serviceController.create(req, res));
 
-router.route("/services").get((req, res) => serviceController.getAll(req, res));
+router // LISTAR TODOS
+    .route("/services").get((req, res) => serviceController.getAll(req, res));
+
+router // LISTAR POR ID
+    .route("/services/:id")
+    .get((req, res) => serviceController.get(req, res));
+
+router // DELETE POR ID
+    .route("/services/:id")
+    .delete((req, res) => serviceController.delete(req, res));
+
+router // ATUALIZA POR ID
+    .route("/services/:id")
+    .put((req, res) => serviceController.update(req, res));
+
 
 module.exports = router;
